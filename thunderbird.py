1c10cc943ea2376cd90342a8480c672ad171502a
subhendup <suvendu.panigrahi@gmail.com>
2016-07-03 15:04:08 +0530

    Add files via upload

---
+++ thunderbird.py
@@ -0,0 +1,24 @@
+import sqlite3 as sql
+import sys
+input= sys.argv[1]
+con = sql.connect('/home/subh/.thunderbird/ip12lpd5.default/global-messages-db.sqlite')
+
+cur=con.cursor()
+cur.execute("select hex(fts3_tokenizer('porter'))")
+
+row = cur.fetchone()
+
+s = "select fts3_tokenizer('mozporter',X'"+ row[0]+"')"
+print s
+cur.execute(s)
+cur.fetchone()
+#cur.execute("select * from messagesText where messagesText MATCH 'monali'")
+cur.execute(input)
+for row in cur:
+    print row
+    n = raw_input('press n to continue: ')
+    if n is 'N' or n is 'n':
+        continue
+    else:
+        sys.exit(0)
+
