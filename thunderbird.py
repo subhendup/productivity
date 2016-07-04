3387321eac097d5effd28d46354c4b6ead9a3849
subhendup <suvendu.panigrahi@gmail.com>
2016-07-04 11:29:08 +0530

    Update thunderbird.py

--- thunderbird.py
+++ thunderbird.py
@@ -1,17 +1,20 @@
 import sqlite3 as sql
 import sys
-input= sys.argv[1]
+# get query as input
+input= sys.argv[1] 
 con = sql.connect('/home/subh/.thunderbird/ip12lpd5.default/global-messages-db.sqlite')
 
 cur=con.cursor()
+#getting the stemmer's hex code
 cur.execute("select hex(fts3_tokenizer('porter'))")
 
 row = cur.fetchone()
-
+#Here , we are setting the fast text tokenizer to the previously mentioned  code.
 s = "select fts3_tokenizer('mozporter',X'"+ row[0]+"')"
 print s
 cur.execute(s)
 cur.fetchone()
+#Print all emails where either body , subject , attachmentNames , author , recipients  contain python
 #cur.execute("select * from messagesText where messagesText MATCH 'python'")
 cur.execute(input)
 for row in cur:
