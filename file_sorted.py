e147e1a23c92c044408baea625cdad64deb360e1
subhendup <suvendu.panigrahi@gmail.com>
2017-11-18 11:24:29 +0530

    Create file_sorted.py

---
+++ file_sorted.py
@@ -0,0 +1,14 @@
+import os
+import re
+import datetime
+
+searchdir = os.environ('PWD')
+allfiles = []
+for root, dir,files in os.walk(searchdir):
+    for f in files:
+        allfiles.append( os.path.join( root,f))
+    #files.sort(key=lambda x: os.path.getmtime(os.path.join( root,x))
+
+allfiles.sort(key=lambda x: os.path.getmtime(x))
+for f in allfiles:
+print f,datetime.datetime.fromtimestamp(os.path.getmtime(f))
